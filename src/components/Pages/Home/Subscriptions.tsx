c2188647bf4b510cb9678fb298657b1535e44dac
Aminulhoque01 <aminuhoque53@gmail.com>
2025-04-13 10:45:03 +0600

    subscrib email

--- src/components/Pages/Home/Subscriptions.tsx
+++ src/components/Pages/Home/Subscriptions.tsx
@@ -6,13 +6,7 @@ export default function Subscribe() {
     <div className="md:px-8">
       {/* Wrapper container with relative positioning to position the circles inside */}
       <div className="w-full md:container bg-[#063B9D] h-full md:h-[160px]  md:px-6 py-5 mx-auto rounded-lg relative">
-        
-        {/* Circles with different sizes and positions */}
-        {/* <div className="absolute left-4 w-20 h-20 bg-[#0741AD] opacity-50 rounded-full"></div>
-        <div className="absolute top-[-1px] right-[1rem] w-20 h-20 bg-[#0741AD] opacity-80 rounded-full"></div> */}
-        {/* <div className="absolute top-5 left-4 w-10 h-10 bg-[#0741AD] opacity-50 rounded-full"></div> */}
-        {/* <div className="absolute top-10 right-16 w-10 h-10 bg-[#0741AD] opacity-50 rounded-full"></div> */}
-
+       
         {/* Content Section */}
         <div className="md:flex-row w-full max-w-md md:max-w-full  px-8">
           <h2 className="text-white text-2xl font-semibold mb-4">
